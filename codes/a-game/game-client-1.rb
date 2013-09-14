6a9ca516945f2e1d4d487b2d2dd27d29e749f804
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-09-14 11:29:13 -0400

    don't use a constant to track state

--- codes/a-game/game-client-1.rb
+++ codes/a-game/game-client-1.rb
@@ -19,9 +19,10 @@ SOCK            = UDPSocket.new.tap{ |s| s.connect(SERVER_IP, SERVER_PORT) }
 GAME_WIN_HEIGHT = 30
 GAME_WIN_WIDTH  = 60
 LOG_WIN_HEIGHT  = 1
-UPDATES         = []
+@game_states   = []
 
 def ship_valid?
+  return false unless SHIP
   SHIP.length == 4 &&
   SHIP.match(/.*[aeiouy].*[aeiouy].*/i)
 end
@@ -60,9 +61,9 @@ end
 # Listen for server state updates
 Thread.new do
   Socket.udp_server_loop(CLIENT_PORT) do |msg, msg_src|
-    new_state = State.new(JSON.parse(msg))
-    old_state = UPDATES.last
-    UPDATES   << new_state
+    new_state    = State.new(JSON.parse(msg))
+    old_state    = @game_states.last
+    @game_states << new_state
     render = Render.new(@win, @log, new_state, old_state, ident)
     render.draw
     render.update_score
